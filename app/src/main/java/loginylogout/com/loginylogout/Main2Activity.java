458165b608d3229275d00385c5578fae3892d901
Daniel <dcamajaig@gmail.com>
2018-04-28 10:21:25 -0600

    tarea numero 2
    prueba de inicio de cession en app

---
+++ app/src/main/java/loginylogout/com/loginylogout/Main2Activity.java
@@ -0,0 +1,32 @@
+package loginylogout.com.loginylogout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+    Button sal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        sal = (Button) findViewById(R.id.sal);
+        sal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+
+
+
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/loginylogout/com/loginylogout/MainActivity.java
@@ -0,0 +1,51 @@
+package loginylogout.com.loginylogout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (PreferenceManager.checkPref(this, PreferenceManager.PREF_SW1)) {
+            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(intent);
+            finish();
+
+        }
+
+            btn = findViewById(R.id.btn1);
+
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String username = ((EditText) findViewById(R.id.user)).getText().toString();
+                    String password = ((EditText) findViewById(R.id.pass)).getText().toString();
+
+                    if (username.equals("edutec") & password.equals("12345")) {
+                        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Usuario Incorrecto", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+            });
+
+
+
+
+    }
+}
